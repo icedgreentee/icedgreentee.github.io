716e06434cfd349905681953852e18e6a778306f
Tanvi Mishra <tanvimishra906@gmail.com>
2025-05-29 18:22:48 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const carousel = document.getElementById("carousel");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
+prevBtn.addEventListener("click", () => {
+  carousel.scrollBy({ left: -320, behavior: "smooth" });
+});
+
+nextBtn.addEventListener("click", () => {
+  carousel.scrollBy({ left: 320, behavior: "smooth" });
+});
